38dc36f78c3716858e2632af516f7836ee6ea9fd
algoritmika14 <algomklass@gmail.com>
2023-03-02 13:30:48 +0500

    sum1

---
+++ Слава/CSharp/Assets/Code/HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Вывод фразы на экране
+        print("Поздравляю! Вы зашли в игру");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Ура победа!");
+    }
+}
